5f81f970097be247b886f9382128ca0122f5f2e0
Enrico-Proietto <enrico.proiettoog@gmail.com>
2023-11-17 10:54:25 +0100

    supabase and android shortcuts

--- myholidaydiary/src/environments/environment.ts
+++ myholidaydiary/src/environments/environment.ts
@@ -1,16 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://unaelnolozfxzvvirfia.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuYWVsbm9sb3pmeHp2dmlyZmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNDQ5ODksImV4cCI6MjAxNTcyMDk4OX0.K6IVmBM-DPIrTRAKufbNjGswU2dg7QrOGJEWa2XdHPo',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
